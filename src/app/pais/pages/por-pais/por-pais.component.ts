442edce5f2fc6e33476d7cf62b0d307a84f1602a
Kevin Ruiz <kevin.ruiz1428@hotmail.com>
2022-12-06 16:32:50 -0600

    Agregar funcionalidad por capital y sugerencias

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -6,6 +6,11 @@ import { PaisService } from '../../services/pais.service';
   selector: 'app-por-pais',
   templateUrl: './por-pais.component.html',
   styles: [
+    `
+    li {
+      cursor: pointer;
+    }
+    `
   ]
 })
 export class PorPaisComponent{
@@ -16,10 +21,16 @@ export class PorPaisComponent{
 
   paises: Country[] = [];
 
+  paisesSugeridos: Country[] = [];
+
+  mostrarSugerencias: boolean = false;
+
   constructor(private paisService: PaisService){
   }
 
   buscar(termino: string){
+
+    this.mostrarSugerencias = false;
     this.hayError = false;
     this.termino = termino
 
@@ -36,6 +47,19 @@ export class PorPaisComponent{
 
   sugerencias(termino:string){
     this.hayError = false;
+
+    this.termino = termino;
+
+    this.mostrarSugerencias = true;
+
+    this.paisService.buscarPais(termino)
+    .subscribe(paises=> this.paisesSugeridos = paises.splice(0,5),
+    (err) => this.paisesSugeridos = []
+    );
+  }
+
+  buscarSugerido(termino: string){
+    this.buscar(termino);
   }
 
 }
